2c84d299b2b5198afe245dfa9371a3bb14403f9b
Todor Belchev <todorbelchev@live.com>
2022-01-04 10:48:43 +0200

    User controller verify and logout unit tests

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -15,6 +15,7 @@ jest.mock('../services/userService', () => {
         getUserByEmail: jest.fn(),
         createUser: jest.fn(),
         editUserById: jest.fn(),
+        getUserById: jest.fn(),
     }
 });
 
@@ -319,5 +320,40 @@ describe('User controller', () => {
             expect(body.message).toEqual('Please enter a valid phone number!');
             expect(editUserByIdMock).toHaveBeenCalledTimes(0);
         });
-    })
+    });
+
+    describe('GET /verify', () => {
+        test('should return empty object if user is not logged in', async () => {
+            const { body, statusCode } = await supertest(app).get('/api/user/verify');
+
+            expect(statusCode).toEqual(200);
+            expect(body).toEqual({});
+        });
+
+        test('should return user details if user is logged in', async () => {
+            const resBody = { _id: '123', email: 'pesho@abv.bg' };
+            const token = createToken({ email: 'pesho@abv.bg', id: '123' });
+            const getUserByIdMock = jest.spyOn(userService, 'getUserById')
+                .mockImplementation(() => Promise.resolve(resBody));
+            const { body, statusCode } = await supertest(app).get('/api/user/verify')
+                .set('Cookie', [`X-Authorization=${token}`]);
+
+            expect(statusCode).toEqual(200);
+            expect(body).toEqual(resBody);
+            expect(getUserByIdMock).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('GET /logout', () => {
+        test('should clear cookie', async () => {
+            const token = createToken({ email: 'pesho@abv.bg', id: '123' });
+            const { body, statusCode, headers } = await supertest(app).get('/api/user/logout')
+                .set('Cookie', [`X-Authorization=${token}`]);
+            const authCookie = headers['set-cookie'].some(x => x.startsWith('X-Authorization='));
+
+            expect(statusCode).toEqual(200);
+            expect(authCookie).toBeDefined();
+            expect(body.message).toEqual('Logged out');
+        });
+    });
 });
